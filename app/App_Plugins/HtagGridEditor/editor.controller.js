fc612fc1adfa6fbd3e2d2dfc0ff4801c67ff582b
Søren Mastrup <sm@oerskov.dk>
2018-09-14 14:49:15 +0200

    Fixed issue where alignment and type wouldn't be transferred between environments

--- app/App_Plugins/HtagGridEditor/editor.controller.js
+++ app/App_Plugins/HtagGridEditor/editor.controller.js
@@ -1,21 +1,30 @@
 ﻿angular.module("umbraco").controller("Our.Umbraco.HtagEditor.Controller", function ($scope) {
-    
+    if ($scope.control.value !== null) {
+        //Handles conversion between editors created in version 1.0.1 to newer
+        if (!angular.isObject($scope.control.value)) {
+            var oldText = $scope.control.value;
+            var oldTag = $scope.control.hTag;
+            var oldAlign = $scope.control.textAlign;
 
-    if (!$scope.control.textAlign)
-    {
-        $scope.control.textAlign = "left";
-    }
-    if (!$scope.control.hTag) {
-        $scope.control.hTag = "h1";
+            $scope.control.value = {
+                "textAlign": oldAlign,
+                "hTag": oldTag,
+                "text": oldText
+            };
+        }
+    } else {
+        $scope.control.value = {
+            "textAlign": "left",
+            "hTag": "h1"
+        };
     }
 
     $scope.setPosition = function (pos) {
-        $scope.control.textAlign = pos;
-    }
+        $scope.control.value.textAlign = pos;
+    };
 
     $scope.setSize = function (hTag) {
-        $scope.control.hTag = hTag;
-    }
+        $scope.control.value.hTag = hTag;
+    };
 
-    
 });
