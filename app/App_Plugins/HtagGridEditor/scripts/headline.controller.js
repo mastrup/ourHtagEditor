b18800eea102e90e7992e9bec06d7ab8f2472e11
Tom Pipe <tom@tompipe.co.uk>
2020-02-25 12:25:27 +0000

    tweak to retrieval of value/defaults

--- app/App_Plugins/HtagGridEditor/scripts/headline.controller.js
+++ app/App_Plugins/HtagGridEditor/scripts/headline.controller.js
@@ -12,6 +12,14 @@
         config = $scope.control.editor.config;
     }
 
+    function setValue(align, size, text) {
+        $scope.control.value = {
+            "textAlign": align ? align : config.options.align.default,
+            "hTag": size ? size : config.options.size.default,
+            "text": text
+        };
+    }
+
     if ($scope.control.value !== null) {
         //Handles conversion between editors created in version 1.0.1 to newer
         if (!angular.isObject($scope.control.value)) {
@@ -19,21 +27,10 @@
             var oldTag = $scope.control.hTag;
             var oldAlign = $scope.control.textAlign;
 
-            $scope.control.value = {
-                "textAlign": oldAlign,
-                "hTag": oldTag,
-                "text": oldText
-            };
+            setValue(oldAlign, oldTag, oldText);
         }
     } else {
-        var defaultAlign = config.options.align.default;
-        var defaultSize = config.options.size.default;
-
-        $scope.control.value = {
-            textAlign: defaultAlign ? defaultAlign : "left",
-            hTag: defaultSize ? defaultSize : "h1",
-            text: ''
-        }
+        setValue();
     }
 
     $scope.sizeOptions = config.options && config.options.size && config.options.size.options ? config.options.size.options : "h1,h2,h3,h4,h5,h6".split(",");
